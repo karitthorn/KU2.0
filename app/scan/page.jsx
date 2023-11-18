07993c3193f5f3eb94b35baeea478ba980fa10e5
karitthorn <56643494+karitthorn@users.noreply.github.com>
2023-11-18 13:13:00 +0700

    add show profile

--- app/scan/page.jsx
+++ app/scan/page.jsx
@@ -27,15 +27,46 @@ export default function ScanPage() {
 
   return (
     <>
-      <div>
+      <div className="md:w-[390px] rounded-sm bg-white ">
         {scanReult ? (
           <>
-            <div className="w-full h-screen">
-              {" "}
-              Success: <p>{scanReult}</p>
-            </div>
-            <div className="fixed bottom-0 w-full">
-              <Navbar />
+            <div className=" m-4">
+              <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+                <div class="flex justify-end px-4 pt-4">
+                  {/* <!-- Dropdown menu --> */}
+                </div>
+                <div class="flex flex-col items-center pb-10">
+                  <img
+                    class="w-24 h-24 mb-3 rounded-full shadow-lg"
+                    src="/docs/images/people/profile-picture-3.jpg"
+                    alt="Bonnie image"
+                  />
+                  <h5 class="mb-1 text-xl font-medium text-gray-900">
+                    นาย ตัวอย่าง
+                  </h5>
+                  <span class="text-sm text-gray-500">
+                    วินมอไซต์ลงทะเบียนเเล้ว
+                  </span>
+                  <div class="flex mt-4 md:mt-6">
+                    <a
+                      href="tel:0-2942-8222"
+                      class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-red-500 rounded-lg hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-blue-300"
+                    >
+                      Call Sos
+                    </a>
+                    <a
+                      href="#"
+                      class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 ms-3"
+                    >
+                      Report
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="fixed bottom-0 w-full">
+                <Navbar />
+              </div>
             </div>
           </>
         ) : (
