be693952fdee46fd6d8e61b556f948a767e16750
karitthorn <56643494+karitthorn@users.noreply.github.com>
2023-11-19 19:22:14 +0700

    add dropdown

--- app/sendedoc/01/forminfo/page.jsx
+++ app/sendedoc/01/forminfo/page.jsx
@@ -3,17 +3,24 @@ import { useEffect, useState } from "react";
 import Navbar from "@/app/components/navbar";
 import Feanav from "@/app/components/feanav";
 
-
 export default function FormInfoPage() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState('');
   const [name, setName] = useState("");
   const [code, setCode] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(name)
-    console.log(code)
+    console.log(name);
+    console.log(code);
+    console.log(selectedOption)
     // Collect data and perform necessary actions
   };
+
+  const handleDropdownChange = (e) => {
+    setSelectedOption(e.target.value);
+  };
+
 
   return (
     <>
@@ -55,6 +62,24 @@ export default function FormInfoPage() {
                 onChange={(e) => setCode(e.target.value)}
               />
             </div>
+{/* -------------- */}
+<label
+                htmlFor="code"
+                className="block mb-2 text-sm font-medium text-gray-900"
+              >
+                ความประสงค์
+              </label>
+<div>
+      <select value={selectedOption} onChange={handleDropdownChange} className=" w-full p-2.5 text-gray-900 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 h-11  mb-3" required>
+      <option value="0">โปรดเลือก</option>
+        <option value="1">ลงทะเบียนล่าช้าหรือรักษาสถานภาพนิสิต</option>
+        <option value="2">เพิ่มหรือถอนรายวิชาล่าช้า</option>
+        <option value="3">ลงทะเบียนเกิน</option>
+        <option value="4">ลงทะเบียนต่ำกว่า 9 หน่วยกิต</option>
+
+      </select>
+    </div>
+{/* --------------------- */}
             <button
               type="submit"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
@@ -65,8 +90,8 @@ export default function FormInfoPage() {
         </section>
       </div>
       <div className="fixed bottom-0 w-full">
-          <Navbar />
-        </div>
+        <Navbar />
+      </div>
     </>
   );
 }
