f29a515f0928220429a9bb714501332e1a919a89
karitthorn <56643494+karitthorn@users.noreply.github.com>
2023-11-20 11:27:40 +0700

    change to number

--- app/sendedoc/01/forminfo/page.jsx
+++ app/sendedoc/01/forminfo/page.jsx
@@ -53,7 +53,7 @@ export default function FormInfoPage() {
                 รหัสนิสิต
               </label>
               <input
-                type="text"
+                type="number"
                 id="code"
                 placeholder="66xxxxxxx"
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
