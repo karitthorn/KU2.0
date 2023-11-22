f05ed5e0dd08ea08cc20a28b4cea455197180164
karitthorn <56643494+karitthorn@users.noreply.github.com>
2023-11-22 15:48:53 +0700

    add photo on extension card

--- app/extensionpage/page.jsx
+++ app/extensionpage/page.jsx
@@ -11,6 +11,7 @@ export default function Home() {
     "https://via.placeholder.com/358x166",
     "https://via.placeholder.com/358x166",
   ]);
+  const [linkurl, setLinkurl] = useState("https%3A%2F%2Fku-nav.vercel.app%2F");
 
   async function getData() {
     try {
@@ -41,52 +42,68 @@ export default function Home() {
 
   return (
     <>
-  <Feanav/>
+      <Feanav />
       <div className="flex flex-col justify-center items-center mt-5 m-4">
         {imageUrls.map((imageUrl, index) => (
           <>
-          <div className="flex max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 mb-2  w-full ">
-          <div className="flex  w-full justify-between ">
-            <div className="flex flex-row">
-              <img
-              key={index}
-                className="   shadow-lg  w-10 h-10 mt-2 mr-2 rounded-full"
+            <div className="flex flex-col max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 mb-2  w-full ">
+            <img
+                key={index}
+                className="w-[358px] h-[166px] rounded-lg mr-2  mb-3"
                 src={imageUrl}
                 alt=""
-                width={50}
-                height={50}
+                width={358}
+                height={166}
               />
-            
-            <div className=" flex flex-col mt-2">
-              <h5 className="  font-bold tracking-tight text-gray-900 leading-5">
-                Ku Nav<br />
-                <span className="font-normal text-gray-700  ">
-                  by <span className=" underline-offset-2">karitthorn</span>
-                </span>
-              </h5>
+              <div className="flex  w-full justify-between ">
+                <div className="flex flex-row">
+                  <img
+                    key={index}
+                    className="   shadow-lg  w-10 h-10 mt-2 mr-2 rounded-full"
+                    src={imageUrl}
+                    alt=""
+                    width={50}
+                    height={50}
+                  />
+
+                  <div className=" flex flex-col mt-2">
+                    <h5 className="  font-bold tracking-tight text-gray-900 leading-5">
+                      Ku Nav
+                      <br />
+                      <span className="font-normal text-gray-700  ">
+                        by{" "}
+                        <span className=" underline-offset-2">karitthorn</span>
+                      </span>
+                    </h5>
+                  </div>
+                </div>
+
+                <div className=" flex justify-start mt-3">
+                  <Link
+                    href={`https://ku-2-0.vercel.app/urldynamic?pdfurl=${linkurl}`}
+                  >
+                    <button
+                      type="button"
+                      class="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 "
+                    >
+                      ใช้งาน
+                    </button>
+                  </Link>
+                </div>
+              </div>
             </div>
-            </div>
-  
-            <div className=" flex justify-start mt-3">
-              <Link href="/extensionpage/addextension">
-                <button
-                  type="button"
-                  class="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 "
-                >
-                  ใช้งาน
-                </button>
-              </Link>
-            </div>
-          </div>
-        </div>
-        </>
+          </>
         ))}
-      <Link className=" text-gray-500 underline-offset-2" href="/extensionpage/addextension">เพิ่ม Extension ของคุณ</Link>
-      <div className="fixed bottom-0 w-full">
+        <Link
+          className=" text-gray-500 underline-offset-2"
+          href="/extensionpage/addextension"
+        >
+          เพิ่ม Extension ของคุณ
+        </Link>
+        <div className="fixed bottom-0 w-full">
           <Navbar />
         </div>
       </div>
     </>
   );
 }
-
