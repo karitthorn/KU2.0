4d038db58b95a92d4b02200e74f38cf1c0ce769b
karitthorn <56643494+karitthorn@users.noreply.github.com>
2023-11-23 20:44:54 +0700

    change exnavbar

--- app/components/exNavber.jsx
+++ app/components/exNavber.jsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-export default function ExNavbar () {
+export default function ExNavbar() {
   const router = useRouter();
   return (
     <>
@@ -26,22 +26,17 @@ export default function ExNavbar () {
               />
             </svg>
           </button>
-            <span className="sr-only"></span>
-            <Link href="extensionpage/addextension">
+          <span className="sr-only"></span>
+          <Link href="extensionpage/addextension">
             <button
               type="button"
               class="text-white bg-cyan-500 hover:bg-cyan-700 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 mt-1"
             >
               Add extension
             </button>
-            </Link>
+          </Link>
 
-          <div
-            className="hidden w-full md:block md:w-auto"
-            id="navbar-dropdown"
-          >
-            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white"></ul>
-          </div>
+
         </div>
       </nav>
     </>
